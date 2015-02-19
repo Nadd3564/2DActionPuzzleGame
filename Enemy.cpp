bdb4bdd6afb1c45253ea859fcd704ba1ff19ffaa
DaikiNakajima3564 <hitanykey.del@gmail.com>
2015-02-19 19:54:46 +0900

    更新@Enemy ・レイヤーへの追加処理を変更

--- Enemy.cpp
+++ Enemy.cpp
@@ -12,18 +12,17 @@
 
 Enemy::Enemy(GameLayer * game) : GameObject(game){}
 
-Enemy::~Enemy(){
-	CC_SAFE_RELEASE(this);
-}
+Enemy::~Enemy(){}
 
 Enemy* Enemy::create(CCPoint position, const char* fileName)
 {
 	//エネミー生成
 	Enemy* enemy = new Enemy(GameLayer::Instance());
-	if (enemy) {
+	if (enemy)
+	{
         enemy->initEnemy(position, fileName);
 		enemy->autorelease();
-		GameLayer::Instance()->setEnemy(enemy);
+		GameLayer::Instance()->addChild(enemy);
 		return enemy;
 	}
 	//autoreleaseを使用しているため、deleteの代わりに使用、メモリを開放
@@ -37,6 +36,7 @@ Enemy* Enemy::initEnemy(CCPoint position, const char* FileName)
 	this->initWithFile(FileName);
 	this->setPosition(position);
 	this->setTag(_gameL->kTag_Enemy);
+	this->setZOrder(_gameL->kOrder_Enemy);
 
 	//物理ボディ生成
 	_body = _gameL->getWorld()->CreateBody(&enemyBodyDef(this));
@@ -57,12 +57,14 @@ Enemy* Enemy::initEnemy(CCPoint position, const char* FileName)
 	return this;
 }
 
-void Enemy::addEnemy(){
+void Enemy::addEnemy()
+{
 	create(ccp(636, 335), "enemy1.png");
 	create(ccp(536, 125), "enemy1.png");
 }
 
-void Enemy::stateUpdate(float dt){
+void Enemy::stateUpdate(float dt)
+{
 	if (this == NULL)
 		return;
     std::cout << "Update for the enemy.";
@@ -72,7 +74,8 @@ void Enemy::stateUpdate(float dt){
 }
 
 //物理ボディ生成
-b2BodyDef Enemy::enemyBodyDef(Enemy* enemy){
+b2BodyDef Enemy::enemyBodyDef(Enemy* enemy)
+{
 	b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
 	bodyDef.position.Set(enemy->getPositionX() / PTM_RATIO,
@@ -82,7 +85,8 @@ b2BodyDef Enemy::enemyBodyDef(Enemy* enemy){
 }
 
 //物理性質
-b2FixtureDef Enemy::enemyFixtureDef(b2Shape* shape){
+b2FixtureDef Enemy::enemyFixtureDef(b2Shape* shape)
+{
 	b2FixtureDef fixtureDef;
     fixtureDef.shape = shape;
 	fixtureDef.filter.categoryBits = 0x0010;
@@ -92,9 +96,10 @@ b2FixtureDef Enemy::enemyFixtureDef(b2Shape* shape){
 	return fixtureDef;
 }
 
-void Enemy::update (float dt) {
-    
-    if (_body && isVisible()) {
+void Enemy::update (float dt)
+{
+    if (_body && isVisible())
+	{
 		setPositionX(_body->GetPosition().x * PTM_RATIO);
 		setPositionY(_body->GetPosition().y * PTM_RATIO);
 		setRotation(CC_RADIANS_TO_DEGREES(-1 * _body->GetAngle()));
